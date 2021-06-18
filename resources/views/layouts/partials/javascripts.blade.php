1e7bc01189a641d74e48c7d6092d4fbcc59cb4c0
Haseeb khan <khanhaseebkhan22@gmail.com>
2021-06-18 17:57:00 +0500

    test

--- resources/views/layouts/partials/javascripts.blade.php
+++ resources/views/layouts/partials/javascripts.blade.php
@@ -151,6 +151,7 @@ setInterval(time, 1000);
         ]
     } );
 } );
+
 </script>
 {{-- <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.7.1/js/dataTables.buttons.min.js"></script>
